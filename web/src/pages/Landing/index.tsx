0ca3d2a22849b5388069c5e7596219c6cb53cccd
Rayan Wilbert <rayan.rmw@gmail.com>
2022-07-30 00:23:54 -0300

    WEB: Landing Page done :white_check_mark:

---
+++ web/src/pages/Landing/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import './styles.css';
+import logoImg from '../../assets/images/logo.svg';
+import landingImg from '../../assets/images/landing.svg';
+import studyIcon from '../../assets/images/icons/study.svg';
+import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
+import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
+
+function Landing() {
+  return (
+    <div id='page-landing'>
+      <div id='page-landing-content' className='container'>
+        <div className='logo-container'>
+          <img src={logoImg} alt='Proffy' />
+          <h2>Your online studies platform.</h2>
+        </div>
+        <img src={landingImg} alt='Studies platform' className='hero-image' />
+        <div className='buttons-container'>
+          <a href='' className='study'>
+            <img src={studyIcon} alt='Study' />
+            Study
+          </a>
+          <a href='' className='give-classes'>
+            <img src={giveClassesIcon} alt='Teach' />
+            Teach
+          </a>
+        </div>
+        <span className='total-connections'>
+          200 connections yet <img src={purpleHeartIcon} alt='Purple Heart' />
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default Landing;
